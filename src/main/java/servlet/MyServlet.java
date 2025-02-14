c34cbaacc10961525d468164be0bf8c3309a0682
mehdi <mehdi.mami@etudiant.univ-rennes.fr>
2025-02-14 16:27:38 +0100

    ajout de lexemple servlet mais pas encore testé

---
+++ src/main/java/servlet/MyServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServlet", urlPatterns = {"/myurl"})
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>GET request received</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>POST request received</h1>");
+        out.println("</body></html>");
+    }
+}
